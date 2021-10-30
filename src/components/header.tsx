2fd8ee240e87c913dd6d4db6cb1e60fb1fa454d3
Tony Lin <tonylin0826@gmail.com>
2021-10-30 00:39:50 -0700

    Update 1. add hint

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,12 @@
 import { AppBar, Toolbar, Typography } from '@mui/material'
+import QrCode2Icon from '@mui/icons-material/QrCode2'
 
 const Header = (): React.ReactElement => {
   return (
     <>
       <AppBar>
         <Toolbar>
+          <QrCode2Icon sx={{ marginRight: 1 }} />
           <Typography variant="h6" component="div">
             QRCoder
           </Typography>
--- src/components/qrcode-list.tsx
+++ src/components/qrcode-list.tsx
@@ -1,4 +1,4 @@
-import { Button, Container, Grid } from '@mui/material'
+import { Button, Container, Grid, Typography } from '@mui/material'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useHistory } from 'react-router'
@@ -6,7 +6,8 @@ import { RootModel } from '../models'
 import { Dispatch } from '../store'
 import { QrCode } from '../types/qrcode'
 import QrCodeComponent from './qrcode'
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline'
+import QrCodeIcon from '@mui/icons-material/QrCode'
 
 const QrCodeList = (): React.ReactElement => {
   const qrCodes = useSelector(
@@ -21,28 +22,98 @@ const QrCodeList = (): React.ReactElement => {
 
   return (
     <Container maxWidth="lg" sx={{ paddingTop: 4 }}>
-      <Grid container spacing={0}>
-        <Button
-          variant="outlined"
-          onClick={() => {
-            const newCode = new QrCode({ url: 'https://example.com' })
-            dispatch.qrCode.saveQrCodes([...qrCodes, newCode])
+      {qrCodes.length > 0 && (
+        <>
+          <Grid
+            container
+            spacing={1}
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Grid item>
+              <Typography variant="h3" sx={{ fontWeight: 'bold' }}>
+                QR Codes
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Button
+                variant="outlined"
+                onClick={() => {
+                  const newCode = new QrCode({ url: 'https://example.com' })
+                  dispatch.qrCode.saveQrCodes([...qrCodes, newCode])
 
-            history.push(`/qrcodes/${newCode.id}`)
-          }}
-        >
-          <AddCircleOutlineIcon sx={{ marginRight: 1 }} />
-          Create a new QRCode
-        </Button>
-      </Grid>
-
-      <Grid container spacing={4} sx={{ paddingTop: 4 }}>
-        {qrCodes.map((code, idx) => (
-          <Grid key={`code${idx}`} item xs={12} sm={12} md={6}>
-            <QrCodeComponent code={code}></QrCodeComponent>
+                  history.push(`/qrcodes/${newCode.id}`)
+                }}
+              >
+                <AddCircleOutlineIcon sx={{ marginRight: 1 }} />
+                Create a new QRCode
+              </Button>
+            </Grid>
           </Grid>
-        ))}
-      </Grid>
+          <Grid container spacing={4} sx={{ paddingTop: 4 }}>
+            {qrCodes.map((code, idx) => (
+              <Grid key={`code${idx}`} item xs={12} sm={12} md={6}>
+                <QrCodeComponent code={code}></QrCodeComponent>
+              </Grid>
+            ))}
+          </Grid>
+        </>
+      )}
+
+      {qrCodes.length === 0 && (
+        <Grid
+          container
+          spacing={4}
+          sx={{ paddingTop: 4, width: '100%', marginTop: 3 }}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid item container xs={12} justifyContent="center">
+            <QrCodeIcon sx={{ width: 320, height: 320, opacity: 0.5 }} />
+          </Grid>
+          <Grid item xs={12} container justifyContent="center">
+            <Typography
+              variant="h4"
+              sx={{ fontWeight: 'bold', textAlign: 'center' }}
+            >
+              Create your first QR Code
+            </Typography>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            container
+            justifyContent="center"
+            sx={{ paddingTop: '16px !important' }}
+          >
+            <Typography
+              variant="body1"
+              sx={{
+                maxWidth: 324,
+                textAlign: 'center',
+                opacity: 0.5,
+              }}
+            >
+              Almost there! Try create a dynamical URL QR Code and play around
+              with it
+            </Typography>
+          </Grid>
+          <Grid item xs={12} container justifyContent="center">
+            <Button
+              variant="contained"
+              onClick={() => {
+                const newCode = new QrCode({ url: 'https://example.com' })
+                dispatch.qrCode.saveQrCodes([...qrCodes, newCode])
+
+                history.push(`/qrcodes/${newCode.id}`)
+              }}
+            >
+              <AddCircleOutlineIcon sx={{ marginRight: 1 }} />
+              Create a new QRCode
+            </Button>
+          </Grid>
+        </Grid>
+      )}
     </Container>
   )
 }
